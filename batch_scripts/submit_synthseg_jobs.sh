b08ed249b425265d555bff23f343f61a5de23dc0
eapostolakis99 <evanapos@bu.edu>
2025-12-07 13:51:39 -0500

    Remove batch_scripts folder

--- batch_scripts/submit_synthseg_jobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in /projectnb/lejlab2/Evangelos_Practice/FreeSurfer/batch_scripts/Original_T1_Scans/*.nii.gz; do
-  base=$(basename "$file" .nii.gz)
-  qsub run_synthseg.qsub "$file" "/projectnb/lejlab2/Evangelos_Practice/FreeSurfer/batch_scripts/SynthSeg_result_files/${base}_SynthSeg.nii.gz"
-done
