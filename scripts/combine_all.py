e3ac1cd6e0a0bacf62eea71de906013a51754781
eapostolakis99 <evanapos@bu.edu>
2026-02-26 01:09:28 -0500

    Updated script to accommodate naming for NE or NR

--- scripts/combine_all.py
+++ scripts/combine_all.py
@@ -6,13 +6,13 @@ from autovalidate.combine import combine_all_components
 # Edit these variables for your environment before running.
 
 # Directory of your study
-#BASE = "path/to/study"
-BASE = "/projectnb/lejlab2/Evangelos_Practice/Autovalidate"
+BASE = "/projectnb/lejlab2/Evangelos_Practice/Test_Autovalidate"
 
 # Inputs
-BRAIN_DIR = os.path.join(BASE, "Extracted_Brains")
-CSF_DIR   = os.path.join(BASE, "Inner_Layer_creation")  # or your latest CSF dir
-SKULL_DIR = os.path.join(BASE, "Inner_Layer_creation")  # or your latest skull dir
+BRAIN_DIR = os.path.join(BASE, "Cleaned")
+CSF_DIR   = os.path.join(BASE, "Cleaned")  # or your latest CSF dir
+SKULL_DIR = os.path.join(BASE, "Cleaned")  # or your latest skull dir
+ORIG_STRAIN_DIR = os.path.join(BASE, "Original_Strain_Scans")  # Define where original strain scans are stored
 
 # Output
 OUT_DIR   = os.path.join(BASE, "Final_Head")
@@ -24,9 +24,9 @@ if __name__ == "__main__":
     # infer subject roots from brain masks
     brain_files = sorted(
         f for f in os.listdir(BRAIN_DIR)
-        if f.endswith("_tMRIreg_T1_SynthSeg_brain_mask.nii.gz")
+        if f.endswith("_brain_clean.nii.gz")
     )
 
     for bf in brain_files:
-        subj_root = bf.replace("_tMRIreg_T1_SynthSeg_brain_mask.nii.gz", "")
-        combine_all_components(BRAIN_DIR, CSF_DIR, SKULL_DIR, OUT_DIR, subj_root)
+        subj_root = bf.replace("_brain_clean.nii.gz", "")
+        combine_all_components(BRAIN_DIR, CSF_DIR, SKULL_DIR, ORIG_STRAIN_DIR, OUT_DIR, subj_root)
